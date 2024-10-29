0fbe2152e2b98191fd788387f98eb75f965dde93
Omri Studio <omri_morin@icloud.com>
2024-10-30 01:50:28 +0200

    add the index

--- boards/shields/nice_view_custom/widgets/status.c
+++ boards/shields/nice_view_custom/widgets/status.c
@@ -94,6 +94,13 @@ static void draw_top(lv_obj_t *widget, lv_color_t cbuf[], const struct status_st
 static void draw_middle(lv_obj_t *widget, lv_color_t cbuf[], const struct status_state *state) {
     lv_obj_t *canvas = lv_obj_get_child(widget, 1);
 
+    lv_draw_label_dsc_t label_dsc_index;
+    init_label_dsc(&label_dsc_index, LVGL_FOREGROUND, &lv_font_montserrat_14, LV_TEXT_ALIGN_CENTER);
+
+    char index_text[3];
+    sprintf(index_text, "%d", state->active_profile_index + 1);
+    lv_canvas_draw_text(canvas, 0, 0, CANVAS_SIZE, &label_dsc_index, index_text);
+
     lv_draw_rect_dsc_t rect_black_dsc;
     init_rect_dsc(&rect_black_dsc, LVGL_BACKGROUND);
     lv_draw_label_dsc_t label_dsc;
